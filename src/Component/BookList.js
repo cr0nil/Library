c06542d68f934cda7bfa5fc68b9e3f3af5ff7b71
LuCkY <luq951@wp.pl>
2020-05-18 20:24:46 +0200

    Add book table

--- src/Component/BookList.js
+++ src/Component/BookList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Frame from 'react-frame-component';
 import Book from './Book';
+import { Table } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 class BookList extends Component {
     constructor(props) {
@@ -10,17 +12,52 @@ class BookList extends Component {
             humor: 'happy'
         }
     }
-    
+
+    renderTableData() {
+        return users.map(user => (
+                <tr>
+                    <td>{user.author}</td>
+                    <td>{user.title}</td>
+                    <td>{user.genre}</td>
+                    <td>{user.isbn}</td>
+                    <td>{user.relase_date}</td>
+                    <td><Button variant="outline-primary">Wypożycz</Button>{' '}</td>
+                </tr>
+            )
+        )
+    }
+
     render() {
         return (
-            <ul>
-                {users.map(user => (
-                    <li key={user.id}>
-                        <Book 
-                        autor = {user.name}/>
-                    </li>
-                ))}
-            </ul>)
+            // <ul>
+            //     {users.map(user => (
+            //         <li key={user.id}>
+            //             <Book
+            //             autor = {user.name}/>
+            //         </li>
+            //     ))}
+            // </ul>
+
+            <div>
+                <h1>Książki</h1>
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>Tytuł</th>
+                        <th>Autor</th>
+                        <th>Gatunek</th>
+                        <th>ISBN</th>
+                        <th>Data wydania</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.renderTableData()}
+                    </tbody>
+                </Table>
+            </div>
+
+
+        )
     }
 }
 export default BookList;
@@ -28,13 +65,19 @@ export default BookList;
 const users = [
     {
         id: 1,
-        name: 'Robin Williams',
-        occupation: 'teacher',
-        //dopisa
+        author: 'Robin Williams',
+        title: 'teacher',
+        genre: 'bajka',
+        isbn: '12345',
+        relase_date: '11-11-1111'
+
     },
     {
         id: 2,
-        name: 'John Doe',
-        occupation: 'gardener',
+        author: 'John Doe',
+        title: 'cos',
+        genre: 'bajka',
+        isbn: '12345',
+        relase_date: '11-11-1111'
     },
 ];
